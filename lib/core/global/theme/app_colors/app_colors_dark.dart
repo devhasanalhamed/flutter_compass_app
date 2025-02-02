15f39cf75235e422750d68e64df8dcf28774b066
Hasan Al-Hamed <therealhassan97@gmail.com>
2025-02-02 18:40:45 +0300

    Create app_colors_dark.dart

---
+++ lib/core/global/theme/app_colors/app_colors_dark.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColorsDark {
+  static const Color white = Color(0xFFF1F1F1);
+  static const Color black = Color(0xFF111111);
+  static const Color red = Color(0xFFF04242);
+  static const Color primaryColor = red;
+  static const Color secondaryColor = white;
+  static const Color primaryTextColor = white;
+  static const Color iconPrimaryColor = red;
+}
